dffc4577ac1bbe3dc5c43e3a00960767596144a7
Ed Salinas <edsalinas7b@yahoo.com>
2016-06-08 19:05:19 +0800

    Hide or show preloader

--- javascripts/custom/main.js
+++ javascripts/custom/main.js
@@ -12,6 +12,20 @@ $(function ($) {
         });
     }
 
+    function showPreloader() {
+        $('#ajax-content').css('opacity', '0');
+        $('.sub-nav-wrap').hide();
+        $('body').addClass('preloader-running');
+    }
+
+    function hidePreloader() {
+        $("#status").fadeOut();
+        $("#preloader").delay(1000).fadeOut(1000);
+        $('body').removeClass('preloader-running');
+        $('body').addClass('preloader-done');
+        $("#ajax-content").delay(1000).css('opacity', '1');
+    }
+
 });
 
 })();
